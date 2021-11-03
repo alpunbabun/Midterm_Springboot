564cbf9ffdde792298346db72de89642eae10a2e
alpunbabun <alfonz.delatorre@ust-legazpi.edu.ph>
2021-11-03 23:52:39 +0800

    Added springboot starter security in dependencies

--- src/main/java/com/example/midterm/MidtermApplication.java
+++ src/main/java/com/example/midterm/MidtermApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MidtermApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MidtermApplication.class, args);
     }
-
 }
